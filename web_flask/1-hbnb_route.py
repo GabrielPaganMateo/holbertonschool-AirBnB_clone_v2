c1145445120311e7d1519a58de18f46a5a1d4c0a
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-03 08:50:53 -0400

    Fix server error

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,10 +12,6 @@ app = Flask(__name__)
 def hello_hbnb():
     return "Hello HBNB!"
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB"
