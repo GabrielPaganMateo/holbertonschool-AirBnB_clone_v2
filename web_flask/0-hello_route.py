5345c8a9af0d0271742b49418e5e5b69e5df7119
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-03 08:33:51 -0400

    FIX STYLE

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,11 +6,12 @@ Script that starts a Flask
 Web app for my HBNB clone project
 """
 app = Flask(__name__)
+
+
 @app.route("/", strict_slashes=False)
-
-
 def hello_hbnb():
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
