2bdb6014027c8e17a48790fe52818a0c92311ec4
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-03 17:54:14 -0400

    Fixed State getter

--- models/state.py
+++ models/state.py
@@ -19,9 +19,8 @@ class State(BaseModel, Base):
         def cities(self):
             from models import storage
             cities = []
-            print(storage.all(City))
             for city in storage.all(City):
                 city_obj = storage.all()[city]
                 if city_obj.__dict__['state_id'] == self.id:
-                    cities.append(city)
+                    cities.append(city_obj)
             return cities
