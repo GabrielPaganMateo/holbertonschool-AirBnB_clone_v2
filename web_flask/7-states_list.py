5f0b91c601dea3c0c06200d6c69192bc1d29a475
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-04 13:51:33 -0400

    mOVE METHODS

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -50,14 +50,16 @@ def number_odd_or_even(n):
     return render_template('6-number_odd_or_even.html', n=n)
 
 
-@app.teardown_appcontext
-def teardown_appcontext(exception):
-    storage.close()
-
 @app.route("/states_list", strict_slashes=False)
 def state_list():
     all_states = storage.all(State)
     return render_template('7-states_list.html', all_states=all_states, header="States")
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
