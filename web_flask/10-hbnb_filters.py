ea7dddfc6e87780ca5f49201c34ceb4d63daa214
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-11 10:42:48 -0400

    Documentation

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,89 +1,25 @@
 #!/usr/bin/python3
-"""This is the module documentation"""
-from flask import Flask
-from flask import render_template
+"""
+Module with Script that starts the 
+HBNB clone flask web application
+"""
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
 from models.amenity import Amenity
 """
 Script that starts a Flask
 Web app for my HBNB clone project
 """
 app = Flask(__name__)
-
-
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def c_is_fun(text):
-    return "C " + f'{text}'.replace("_", " ")
-
-
-@app.route("/python/", strict_slashes=False, defaults={'text': 'is cool'})
-@app.route("/python/<text>", strict_slashes=False)
-def python(text):
-    if text == "is cool":
-        return "Python is cool"
-    else:
-        return "Python " + f'{text}'.replace("_", " ")
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def number(n):
-    return f'{n} is a number'
-
-
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def number_template(n):
-    return render_template('5-number.html', n=n)
-
-
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def number_odd_or_even(n):
-    return render_template('6-number_odd_or_even.html', n=n)
-
-
-@app.route("/states_list", strict_slashes=False)
-def state_list():
-    all_states = storage.all(State).values()
-    return render_template('7-states_list.html',
-                           all_states=all_states, header="States")
-
-
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_state():
-    all_states = storage.all(State).values()
-    return render_template('8-cities_by_states.html', all_states=all_states,
-                           header="States")
-
-
-@app.route("/states", strict_slashes=False)
-@app.route("/states/<id>", strict_slashes=False)
-def states(id=None):
-    if id:
-        try:
-            state = storage.all(State)[f'State.{id}']
-            return render_template('9-states.html', state=state,
-                                   header="States", id=id)
-        except KeyError:
-            return render_template('9-states.html', error="Error",)
-    else:
-        all_states = storage.all(State).values()
-        return render_template('9-states.html',
-                               all_states=all_states, header="States")
     
 
 @app.route("/hbnb_filters", strict_slashes=False)
 def displayHBNB():
+    """
+    Renders the HTML of HBNB clone project,
+    with database information included
+    """
     states = storage.all(State).values()
     amenities = storage.all(Amenity).values()
     return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
@@ -91,6 +27,7 @@ def displayHBNB():
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
+    """Removes the current SQAlchemy Session"""
     storage.close()
 
 
